709b946cf4fc3b048e90bc0824ab634b14753c30
Chris Sullivan <christopsullivan@outlook.com>
2022-08-13 18:39:15 -0700

    test

--- src/test/java/com/revature/testrunner/testrunner.java
+++ src/test/java/com/revature/testrunner/testrunner.java
@@ -24,7 +24,7 @@ public class testrunner extends AbstractTestNGCucumberTests{
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
 //        options.addArguments("−−incognito");
-        options.addArguments("--headless", "--window-size=1920,1080");
+        options.addArguments("--incognito");
 
         driver = new ChromeDriver(options);
     }
